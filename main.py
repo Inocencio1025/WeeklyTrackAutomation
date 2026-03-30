e45e6dd9f61a7c37771498573cd97e1123c1226c
R. Inocencio <Inocencio@Oakland.edu>
2026-03-30 16:02:22 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from pytube import Playlist, YouTube
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
-url = "https://www.youtube.com/playlist?list=PLP4CSgl7K7or84AAhr7zlLNpghEnKWu2c"
-pl = Playlist(url)
+# scopes needed: read playlist items + modify your playlists
+SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
-# get the latest video (first in the list)
-latest_video_url = pl.video_urls[0]
-video = YouTube(latest_video_url)
+# OAuth flow
+flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+credentials = flow.run_console()
 
-print("Title:", video.title)
-print("Upload date:", video.publish_date)
-print("Description (first 300 chars):")
-print(video.description[:300])
+youtube = build("youtube", "v3", credentials=credentials)
+
+# replace with Fantano playlist ID
+playlist_id = "PLP4CSgl7K7or84AAhr7zlLNpghEnKWu2c"
+
+# fetch latest video
+request = youtube.playlistItems().list(
+    part="snippet",
+    playlistId=playlist_id,
+    maxResults=1
+)
+response = request.execute()
+
+video = response["items"][0]["snippet"]
+print("Title:", video["title"])
+print("Published at:", video["publishedAt"])
+print("Video ID:", video["resourceId"]["videoId"])
